d5c036ec00cb1a2a3d68d33adc7d2419aa050ff3
ladyterrelle <73183578+ladyterrelle@users.noreply.github.com>
2020-12-16 12:46:30 -0500

    Add files via upload

---
+++ TechTalentTwitter/src/main/java/com/tts/TechTalentTwitter/model/Role.java
@@ -0,0 +1,24 @@
+package com.tts.TechTalentTwitter.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Role {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Long id;
+    private String role;
+}
